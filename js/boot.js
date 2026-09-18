e34eba69bc4599a8190a9c9742c763a2526c125e
Swoop081 <justinbelot8@gmail.com>
2026-09-18 11:14:08 +1000

    v0.6.67 position parser release sync js/boot.js

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.66';
+const VERSION='0.6.67';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
