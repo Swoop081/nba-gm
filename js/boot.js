a8758bb7e956215834c923c803a74316b0a8a7ac
Swoop081 <justinbelot8@gmail.com>
2026-09-21 08:23:57 +1000

    v0.6.174 rerender home after day advance: js/boot.js

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.173';
+const VERSION='0.6.174';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
