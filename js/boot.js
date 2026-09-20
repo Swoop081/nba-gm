2887035ea72227dac45b2390b5a83919935bb817
Swoop081 <justinbelot8@gmail.com>
2026-09-21 09:38:55 +1000

    v0.6.176 parser hotfix sync: js/boot.js

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.175';
+const VERSION='0.6.176';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
