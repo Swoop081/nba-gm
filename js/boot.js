bc1d566038e370183c9306bf616fa95e406cc6a6
Swoop081 <justinbelot8@gmail.com>
2026-09-21 07:39:26 +1000

    v0.6.168 match roster portrait framing: js/boot.js

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.167';
+const VERSION='0.6.168';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
