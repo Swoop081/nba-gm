7431b773f7afa012b6d64fe164fb89d3fd6ba229
Swoop081 <justinbelot8@gmail.com>
2026-09-21 22:08:28 +1000

    Release v0.6.219 redesigned League hub

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.218';
+const VERSION='0.6.219';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
