d0e39a8cbe14635875bd751d7397bd8b7d0721e9
Swoop081 <justinbelot8@gmail.com>
2026-09-21 22:01:22 +1000

    Release v0.6.218 quarter-by-quarter box scoring

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.217';
+const VERSION='0.6.218';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
