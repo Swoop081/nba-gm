5ac117605cb2b44d01221ef33d0e43927df76337
Swoop081 <justinbelot8@gmail.com>
2026-09-21 20:56:54 +1000

    Release v0.6.210 enforce persisted postgame date

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.209';
+const VERSION='0.6.210';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
