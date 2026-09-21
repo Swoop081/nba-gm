afe74c68faa1965341526690337e2ad887c12ca3
Swoop081 <justinbelot8@gmail.com>
2026-09-21 21:21:51 +1000

    Release v0.6.213 single calendar authority and stable Cup IDs

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.212';
+const VERSION='0.6.213';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
