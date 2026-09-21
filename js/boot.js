aa78dd1ec33c3972591369a65c8cedfdb5f14d52
Swoop081 <justinbelot8@gmail.com>
2026-09-21 15:39:35 +1000

    v0.6.189 release sync js/boot.js

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.188';
+const VERSION='0.6.189';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
