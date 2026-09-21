fa2eeaefbe9818fa58a2e34aa406d2fa7f9750a3
Swoop081 <justinbelot8@gmail.com>
2026-09-22 09:07:15 +1000

    Add complete NBA Finals history 1947-2026

---
+++ data/nba-finals-history.js
@@ -0,0 +1,483 @@
+// NBA/BAA Finals history through 2025-26. Source: NBA.com history-nba-champions, audited 22 Sep 2026.
+export const NBA_FINALS_HISTORY=[
+  {
+    "season": "2025-26",
+    "champion": "New York Knicks",
+    "runnerUp": "San Antonio Spurs",
+    "series": "4-1"
+  },
+  {
+    "season": "2024-25",
+    "champion": "Oklahoma City Thunder",
+    "runnerUp": "Indiana Pacers",
+    "series": "4-3"
+  },
+  {
+    "season": "2023-24",
+    "champion": "Boston Celtics",
+    "runnerUp": "Dallas Mavericks",
+    "series": "4-1"
+  },
+  {
+    "season": "2022-23",
+    "champion": "Denver Nuggets",
+    "runnerUp": "Miami Heat",
+    "series": "4-1"
+  },
+  {
+    "season": "2021-22",
+    "champion": "Golden State Warriors",
+    "runnerUp": "Boston Celtics",
+    "series": "4-2"
+  },
+  {
+    "season": "2020-21",
+    "champion": "Milwaukee Bucks",
+    "runnerUp": "Phoenix Suns",
+    "series": "4-2"
+  },
+  {
+    "season": "2019-20",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Miami Heat",
+    "series": "4-2"
+  },
+  {
+    "season": "2018-19",
+    "champion": "Toronto Raptors",
+    "runnerUp": "Golden State Warriors",
+    "series": "4-2"
+  },
+  {
+    "season": "2017-18",
+    "champion": "Golden State Warriors",
+    "runnerUp": "Cleveland Cavaliers",
+    "series": "4-0"
+  },
+  {
+    "season": "2016-17",
+    "champion": "Golden State Warriors",
+    "runnerUp": "Cleveland Cavaliers",
+    "series": "4-1"
+  },
+  {
+    "season": "2015-16",
+    "champion": "Cleveland Cavaliers",
+    "runnerUp": "Golden State Warriors",
+    "series": "4-3"
+  },
+  {
+    "season": "2014-15",
+    "champion": "Golden State Warriors",
+    "runnerUp": "Cleveland Cavaliers",
+    "series": "4-2"
+  },
+  {
+    "season": "2013-14",
+    "champion": "San Antonio Spurs",
+    "runnerUp": "Miami Heat",
+    "series": "4-1"
+  },
+  {
+    "season": "2012-13",
+    "champion": "Miami Heat",
+    "runnerUp": "San Antonio Spurs",
+    "series": "4-3"
+  },
+  {
+    "season": "2011-12",
+    "champion": "Miami Heat",
+    "runnerUp": "Oklahoma City Thunder",
+    "series": "4-1"
+  },
+  {
+    "season": "2010-11",
+    "champion": "Dallas Mavericks",
+    "runnerUp": "Miami Heat",
+    "series": "4-2"
+  },
+  {
+    "season": "2009-10",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Boston Celtics",
+    "series": "4-3"
+  },
+  {
+    "season": "2008-09",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Orlando Magic",
+    "series": "4-1"
+  },
+  {
+    "season": "2007-08",
+    "champion": "Boston Celtics",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-2"
+  },
+  {
+    "season": "2006-07",
+    "champion": "San Antonio Spurs",
+    "runnerUp": "Cleveland Cavaliers",
+    "series": "4-0"
+  },
+  {
+    "season": "2005-06",
+    "champion": "Miami Heat",
+    "runnerUp": "Dallas Mavericks",
+    "series": "4-2"
+  },
+  {
+    "season": "2004-05",
+    "champion": "San Antonio Spurs",
+    "runnerUp": "Detroit Pistons",
+    "series": "4-3"
+  },
+  {
+    "season": "2003-04",
+    "champion": "Detroit Pistons",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-1"
+  },
+  {
+    "season": "2002-03",
+    "champion": "San Antonio Spurs",
+    "runnerUp": "New Jersey Nets",
+    "series": "4-2"
+  },
+  {
+    "season": "2001-02",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "New Jersey Nets",
+    "series": "4-0"
+  },
+  {
+    "season": "2000-01",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Philadelphia 76ers",
+    "series": "4-1"
+  },
+  {
+    "season": "1999-00",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Indiana Pacers",
+    "series": "4-2"
+  },
+  {
+    "season": "1998-99",
+    "champion": "San Antonio Spurs",
+    "runnerUp": "New York Knicks",
+    "series": "4-1"
+  },
+  {
+    "season": "1997-98",
+    "champion": "Chicago Bulls",
+    "runnerUp": "Utah Jazz",
+    "series": "4-2"
+  },
+  {
+    "season": "1996-97",
+    "champion": "Chicago Bulls",
+    "runnerUp": "Utah Jazz",
+    "series": "4-2"
+  },
+  {
+    "season": "1995-96",
+    "champion": "Chicago Bulls",
+    "runnerUp": "Seattle SuperSonics",
+    "series": "4-2"
+  },
+  {
+    "season": "1994-95",
+    "champion": "Houston Rockets",
+    "runnerUp": "Orlando Magic",
+    "series": "4-0"
+  },
+  {
+    "season": "1993-94",
+    "champion": "Houston Rockets",
+    "runnerUp": "New York Knicks",
+    "series": "4-3"
+  },
+  {
+    "season": "1992-93",
+    "champion": "Chicago Bulls",
+    "runnerUp": "Phoenix Suns",
+    "series": "4-2"
+  },
+  {
+    "season": "1991-92",
+    "champion": "Chicago Bulls",
+    "runnerUp": "Portland Trail Blazers",
+    "series": "4-2"
+  },
+  {
+    "season": "1990-91",
+    "champion": "Chicago Bulls",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-1"
+  },
+  {
+    "season": "1989-90",
+    "champion": "Detroit Pistons",
+    "runnerUp": "Portland Trail Blazers",
+    "series": "4-1"
+  },
+  {
+    "season": "1988-89",
+    "champion": "Detroit Pistons",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-0"
+  },
+  {
+    "season": "1987-88",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Detroit Pistons",
+    "series": "4-3"
+  },
+  {
+    "season": "1986-87",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Boston Celtics",
+    "series": "4-2"
+  },
+  {
+    "season": "1985-86",
+    "champion": "Boston Celtics",
+    "runnerUp": "Houston Rockets",
+    "series": "4-2"
+  },
+  {
+    "season": "1984-85",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Boston Celtics",
+    "series": "4-2"
+  },
+  {
+    "season": "1983-84",
+    "champion": "Boston Celtics",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-3"
+  },
+  {
+    "season": "1982-83",
+    "champion": "Philadelphia 76ers",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-0"
+  },
+  {
+    "season": "1981-82",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Philadelphia 76ers",
+    "series": "4-2"
+  },
+  {
+    "season": "1980-81",
+    "champion": "Boston Celtics",
+    "runnerUp": "Houston Rockets",
+    "series": "4-2"
+  },
+  {
+    "season": "1979-80",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "Philadelphia 76ers",
+    "series": "4-2"
+  },
+  {
+    "season": "1978-79",
+    "champion": "Seattle SuperSonics",
+    "runnerUp": "Washington Bullets",
+    "series": "4-1"
+  },
+  {
+    "season": "1977-78",
+    "champion": "Washington Bullets",
+    "runnerUp": "Seattle SuperSonics",
+    "series": "4-3"
+  },
+  {
+    "season": "1976-77",
+    "champion": "Portland Trail Blazers",
+    "runnerUp": "Philadelphia 76ers",
+    "series": "4-2"
+  },
+  {
+    "season": "1975-76",
+    "champion": "Boston Celtics",
+    "runnerUp": "Phoenix Suns",
+    "series": "4-2"
+  },
+  {
+    "season": "1974-75",
+    "champion": "Golden State Warriors",
+    "runnerUp": "Washington Bullets",
+    "series": "4-0"
+  },
+  {
+    "season": "1973-74",
+    "champion": "Boston Celtics",
+    "runnerUp": "Milwaukee Bucks",
+    "series": "4-3"
+  },
+  {
+    "season": "1972-73",
+    "champion": "New York Knicks",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-1"
+  },
+  {
+    "season": "1971-72",
+    "champion": "Los Angeles Lakers",
+    "runnerUp": "New York Knicks",
+    "series": "4-1"
+  },
+  {
+    "season": "1970-71",
+    "champion": "Milwaukee Bucks",
+    "runnerUp": "Baltimore Bullets",
+    "series": "4-0"
+  },
+  {
+    "season": "1969-70",
+    "champion": "New York Knicks",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-3"
+  },
+  {
+    "season": "1968-69",
+    "champion": "Boston Celtics",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-3"
+  },
+  {
+    "season": "1967-68",
+    "champion": "Boston Celtics",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-2"
+  },
+  {
+    "season": "1966-67",
+    "champion": "Philadelphia 76ers",
+    "runnerUp": "San Francisco Warriors",
+    "series": "4-2"
+  },
+  {
+    "season": "1965-66",
+    "champion": "Boston Celtics",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-3"
+  },
+  {
+    "season": "1964-65",
+    "champion": "Boston Celtics",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-1"
+  },
+  {
+    "season": "1963-64",
+    "champion": "Boston Celtics",
+    "runnerUp": "San Francisco Warriors",
+    "series": "4-1"
+  },
+  {
+    "season": "1962-63",
+    "champion": "Boston Celtics",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-2"
+  },
+  {
+    "season": "1961-62",
+    "champion": "Boston Celtics",
+    "runnerUp": "Los Angeles Lakers",
+    "series": "4-3"
+  },
+  {
+    "season": "1960-61",
+    "champion": "Boston Celtics",
+    "runnerUp": "St. Louis Hawks",
+    "series": "4-1"
+  },
+  {
+    "season": "1959-60",
+    "champion": "Boston Celtics",
+    "runnerUp": "St. Louis Hawks",
+    "series": "4-3"
+  },
+  {
+    "season": "1958-59",
+    "champion": "Boston Celtics",
+    "runnerUp": "Minneapolis Lakers",
+    "series": "4-0"
+  },
+  {
+    "season": "1957-58",
+    "champion": "St. Louis Hawks",
+    "runnerUp": "Boston Celtics",
+    "series": "4-2"
+  },
+  {
+    "season": "1956-57",
+    "champion": "Boston Celtics",
+    "runnerUp": "St. Louis Hawks",
+    "series": "4-3"
+  },
+  {
+    "season": "1955-56",
+    "champion": "Philadelphia Warriors",
+    "runnerUp": "Fort Wayne Pistons",
+    "series": "4-1"
+  },
+  {
+    "season": "1954-55",
+    "champion": "Syracuse Nationals",
+    "runnerUp": "Fort Wayne Pistons",
+    "series": "4-3"
+  },
+  {
+    "season": "1953-54",
+    "champion": "Minneapolis Lakers",
+    "runnerUp": "Syracuse Nationals",
+    "series": "4-3"
+  },
+  {
+    "season": "1952-53",
+    "champion": "Minneapolis Lakers",
+    "runnerUp": "New York Knicks",
+    "series": "4-1"
+  },
+  {
+    "season": "1951-52",
+    "champion": "Minneapolis Lakers",
+    "runnerUp": "New York Knicks",
+    "series": "4-1"
+  },
+  {
+    "season": "1950-51",
+    "champion": "Rochester Royals",
+    "runnerUp": "New York Knicks",
+    "series": "4-3"
+  },
+  {
+    "season": "1949-50",
+    "champion": "Minneapolis Lakers",
+    "runnerUp": "Syracuse Nationals",
+    "series": "4-2"
+  },
+  {
+    "season": "1948-49",
+    "champion": "Minneapolis Lakers",
+    "runnerUp": "Washington Capitols",
+    "series": "4-2"
+  },
+  {
+    "season": "1947-48",
+    "champion": "Baltimore Bullets",
+    "runnerUp": "Philadelphia Warriors",
+    "series": "4-2"
+  },
+  {
+    "season": "1946-47",
+    "champion": "Philadelphia Warriors",
+    "runnerUp": "Chicago Stags",
+    "series": "4-1"
+  }
+];
