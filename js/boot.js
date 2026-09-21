b9855e1cfe1827faf6e6a9c4e00114dad3243f63
Swoop081 <justinbelot8@gmail.com>
2026-09-22 07:20:58 +1000

    Release v0.6.225 preserve Home news outside async season hub

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.224';
+const VERSION='0.6.225';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
