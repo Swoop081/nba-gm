c3595726b24dac65f8d8a413e439b95665ea78a2
Swoop081 <justinbelot8@gmail.com>
2026-09-22 10:07:36 +1000

    v0.6.253 version bump

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.252';
+const VERSION='0.6.253';
 const CACHE=`nba-gm-${VERSION}`;
 self.addEventListener('install',event=>{self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k.startsWith('nba-gm-')&&k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})())});
