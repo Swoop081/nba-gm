62d29d18b0bfe53d07794cd98428ea835e334ea7
Swoop081 <justinbelot8@gmail.com>
2026-09-22 16:16:40 +1000

    Bump syntax repair to v0.6.285

--- js/boot.js
+++ js/boot.js
@@ -1,4 +1,4 @@
-const VERSION='0.6.284';
+const VERSION='0.6.285';
 const BUILD_KEY='nba-gm-build';
 const versionEl=document.querySelector('[data-version]');if(versionEl)versionEl.textContent=`VERSION ${VERSION}`;
 try{localStorage.setItem(BUILD_KEY,VERSION)}catch{}
